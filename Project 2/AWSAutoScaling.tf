c5474fb22968fcc45b1d8dc99a0254b6f98651b4
Daniel H <42782636+hirahokad@users.noreply.github.com>
2018-09-27 11:18:52 -0700

    Update AWSAutoScaling.tf

--- Project 2/AWSAutoScaling.tf
+++ Project 2/AWSAutoScaling.tf
@@ -47,3 +47,23 @@ EOF
     propagate_at_launch = false
   }
 }
+
+
+
+##This section can maybe substitute a section above
+resource "aws_launch_template" "foobar" {
+  name_prefix = "foobar"
+  image_id = "ami-1a2b3c"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity = 1
+  max_size = 1
+  min_size = 1
+  launch_template = {
+    id = "${aws_launch_template.foobar.id}"
+    version = "$$Latest"
+  }
+}
