cd322cede9ff9c93d2a84c1f68b18aecd4d84347
Daniel H <42782636+hirahokad@users.noreply.github.com>
2018-10-20 23:07:13 -0700

    Creating-staging.py

---
+++ project3/terraformfiles/lambda/update-staging.py
@@ -0,0 +1,19 @@
+import boto3
+from pprint import pprint
+client = boto3.client('ecs')
+
+
+def main(event, context):
+    response = client.update_service(
+        cluster='zephyrecscluster',
+        service='test-http',
+        desiredCount=1,
+        taskDefinition='task1:8',
+        deploymentConfiguration={
+            'maximumPercent': 200,
+            'minimumHealthyPercent': 100
+        },
+        platformVersion='null',
+        forceNewDeployment=True,
+        healthCheckGracePeriodSeconds=60
+    )
